e28f9758fe0b69e29d2fffea72de9a8926e475fb
murtazasoomro <murtazasoomro@gmail.com>
2025-03-19 01:15:43 -0700

    Header Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Header from "../../components/Header";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      {/* Head component for meta tags and title */}
+
+      <Header></Header>
+
+
+      {/* Body */}
+      {/* Footer */}
+    </div>
+  );
 }
+
